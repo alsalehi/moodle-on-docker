d476b2eeffa9e8876e0488c130dd681df88a3e0c
Sameer AlSalehi <89190172+alsalehi@users.noreply.github.com>
2026-08-13 06:12:36 +0300

    Update Memcached session handler configuration

--- scripts/moodle-config.php
+++ scripts/moodle-config.php
@@ -234,12 +234,13 @@ $CFG->admin = 'admin';
 //      $CFG->session_handler_class = '\core\session\file';
 //      $CFG->session_file_save_path = $CFG->dataroot.'/sessions';
 //
-//   Memcached session handler (requires memcached server and extension):
-//      $CFG->session_handler_class = '\core\session\memcached';
-//      $CFG->session_memcached_save_path = '127.0.0.1:11211';
-//      $CFG->session_memcached_prefix = 'memc.sess.key.';
-//      $CFG->session_memcached_acquire_lock_timeout = 120;
-//      $CFG->session_memcached_lock_expire = 7200;       // Ignored if memcached extension <= 2.1.0
+  // Memcached session handler (requires memcached server and extension):
+     $CFG->session_handler_class = '\core\session\memcached';
+     $CFG->session_memcached_save_path = 'memcached:11211';
+     $CFG->cachestore_memcached_servers = 'memcached:11211';
+     $CFG->session_memcached_prefix = 'memc.sess.key.';
+     $CFG->session_memcached_acquire_lock_timeout = 120;
+     $CFG->session_memcached_lock_expire = 7200;       // Ignored if memcached extension <= 2.1.0
 //
 //   Memcache session handler (requires memcached server and memcache extension):
 //      $CFG->session_handler_class = '\core\session\memcache';
